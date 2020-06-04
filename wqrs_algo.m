a473a6ce7cbc6b8d5bcd19ccd8831d16d4dc7673
Ziti481122 <54978570+Ziti481122@users.noreply.github.com>
2020-06-04 13:32:50 +0900

    Update wqrs_algo.m

--- wqrs_algo.m
+++ wqrs_algo.m
@@ -3,8 +3,13 @@
 %input: signal, fs
 %output:amplitude and index of QRS
 %requirement= wfdb toolbox from physionet website
+%
+%Notes: 
+%The WQRS algorithm is written in C and included in the WFDB Software Package 
+%and can be downloaded as source code (C/C++) from PhysioNet website.
 %The WFDB Toolbox provides access from MATLAB to WQRS applications 
-%in the WFDB Software Package in Physionet website.
+%in the WFDB Software Package.
+%This file is used to access the WQRS algorithm.
 
 clear all;
 clc;
